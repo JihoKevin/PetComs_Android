12829c342a2d04dd0348bc4570d4543dc69183e3
jihoKevin <jhshin925@gmail.com>
2021-07-18 02:05:31 +0900

    각종 패키지 및 retrofit 클래스 구현

--- app/src/main/java/kevin/android/petcoms/fragments/network/Client.kt
+++ app/src/main/java/kevin/android/petcoms/fragments/network/Client.kt
@@ -1,6 +1,30 @@
 package kevin.android.petcoms.fragments.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 object Client { // 백엔드 웹 서버와  retrofit을 사용 할 예정
 
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com/" // retrofit 통신을 확인하기 위한 Fake Api 사용. 임시 API
+    //private const val BASE_URL = "http://10.0.2.2:8080/" -> localhost로 통신 할 때는 이걸로 사용하세요~
+
+    val retrofitService: NetworkApi
+
+    init{
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val logger = OkHttpClient.Builder().addInterceptor(interceptor).readTimeout(20, TimeUnit.SECONDS).writeTimeout(20, TimeUnit.SECONDS).build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(logger)
+            .build()
+
+        retrofitService = retrofit.create(NetworkApi::class.java)
+    }
 
 }
---
+++ app/src/main/java/kevin/android/petcoms/repository/PetComsRepository.kt
@@ -0,0 +1,4 @@
+package kevin.android.petcoms.repository
+
+class PetComsRepository {
+}

9a456a1aa668034d0033f8f363027c05a089bbe5
sigooklee <lee960405@gmail.com>
2021-08-01 17:24:16 +0900

    마이페이지 데이터 바인딩 시도 리사이클러뷰 어댑터 오류 그래들 extension 제거

--- app/src/main/java/kevin/android/petcoms/fragments/mypage/MyPageFragment.kt
+++ app/src/main/java/kevin/android/petcoms/fragments/mypage/MyPageFragment.kt
@@ -12,26 +12,24 @@ import kevin.android.petcoms.R
 import kevin.android.petcoms.databinding.FragmentMypageBinding
 import kevin.android.petcoms.fragments.mypage.adapter.MyPetsAdapter
 import kevin.android.petcoms.fragments.mypage.viewmodel.MyPageViewModel
-import kotlinx.android.synthetic.main.fragment_mypage.*
-import kotlinx.android.synthetic.main.fragment_mypage.view.*
 
 class MyPageFragment : Fragment() {
 
-    private var fragmentMypageBinding : FragmentMypageBinding? = null
-    private lateinit var myPageViewModel: MyPageViewModel
+//    private var fragmentMypageBinding : FragmentMypageBinding? = null
+//    private lateinit var myPageViewModel: MyPageViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         // Inflate the layout for this fragment
-//        val view = inflater.inflate(R.layout.fragment_mypage, container, false)
-        val binding = FragmentMypageBinding.inflate(inflater, container, false)
-        fragmentMypageBinding = binding
+        val view = inflater.inflate(R.layout.fragment_mypage, container, false)
+//        val binding = FragmentMypageBinding.inflate(inflater, container, false)
+//        fragmentMypageBinding = binding
 
-        myPageViewModel = ViewModelProvider(this).get(MyPageViewModel::class.java)
+//        myPageViewModel = ViewModelProvider(this).get(MyPageViewModel::class.java)
 //        binding.viewModel = myPageViewModel
-        myPageViewModel.myPetsList.observe(viewLifecycleOwner, Observer{
-            it
-        })
+//        myPageViewModel.myPetsList.observe(viewLifecycleOwner, Observer{
+//            it
+//        })
 
 //        val adapter = MyPetsAdapter()
 //        val adapter = recyclerView.adapter as MyPetsAdapter
@@ -39,8 +37,8 @@ class MyPageFragment : Fragment() {
 //        rv_pets.adapter = adapter
 //        rv_pets.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
-//        return view
-        return fragmentMypageBinding!!.root
+        return view
+//        return fragmentMypageBinding!!.root
     }
 
 }

cffb59312fc3f163c8161157cd3cc859407253cc
JihoKevin <jhshin925@gmail.com>
2021-12-19 00:43:57 +0900

    Calendar Sample complete

--- app/src/main/java/kevin/android/petcoms/fragments/record/RecordFragment.kt
+++ app/src/main/java/kevin/android/petcoms/fragments/record/RecordFragment.kt
@@ -20,6 +20,7 @@ import kevin.android.petcoms.databinding.FragmentRecordBinding
 import kevin.android.petcoms.fragments.record.viewmodel.RecordViewModel
 import kotlinx.android.synthetic.main.fragment_record.*
 import java.time.DayOfWeek
+import java.time.LocalDate
 import java.time.YearMonth
 import java.time.temporal.WeekFields
 import java.util.*
@@ -32,47 +33,66 @@ class RecordFragment : PetComsBaseFragment<FragmentRecordBinding>(R.layout.fragm
     val firstMonth = currentMonth.minusMonths(10)
     val lastMonth = currentMonth.plusMonths(10)
     val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+    private var selectedDate: LocalDate? = null
 
-    val daysOfWeek = arrayOf(
-        DayOfWeek.SUNDAY,
-        DayOfWeek.MONDAY,
-        DayOfWeek.TUESDAY,
-        DayOfWeek.WEDNESDAY,
-        DayOfWeek.THURSDAY,
-        DayOfWeek.FRIDAY,
-        DayOfWeek.SATURDAY
-    )
+    val daysOfWeek = daysOfWeekFromLocale()
 // Use the daysOfWeek to set up your month header texts:
 // Sun | Mon | Tue | Wed | Thu | Fri | Sat
 
-
-
     override fun initViews(view: View) {
         txtTest.setOnClickListener {
-            viewModel.getComments(2)
+            //viewModel.getComments(2)
+            //calendarView.notifyMonthChanged()
+
+            //calendarView.notifyCalendarChanged()
         }
 
-        calendarView.dayBinder = object : DayBinder<DayViewContainer>{
-            // 새로운container 필요 시 호출됨
+        calendarView.dayBinder = object : DayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
             override fun bind(container: DayViewContainer, day: CalendarDay) {
-                container.textView.text = day.date.dayOfMonth.toString()
+                container.day = day
+                val textView = container.textView
+                textView.text = day.date.dayOfMonth.toString()
                 if (day.owner == DayOwner.THIS_MONTH) {
-                    container.textView.setTextColor(Color.BLACK)
+                    // Show the month dates. Remember that views are recycled!
+                    textView.visibility = View.VISIBLE
+                    if (day.date == selectedDate) {
+                        // If this is the selected date, show a round background and change the text color.
+                        textView.setTextColor(Color.WHITE)
+                        textView.setBackgroundResource(R.drawable.ic_launcher_background)
+                    } else {
+                        // If this is NOT the selected date, remove the background and reset the text color.
+                        textView.setTextColor(Color.BLACK)
+                        textView.background = null
+                    }
                 } else {
-                    container.textView.setTextColor(Color.GRAY)
+                    // Hide in and out dates
+                    textView.visibility = View.INVISIBLE
                 }
             }
-
-            // 재사용 용도의 bind
-            override fun create(view: View): DayViewContainer = DayViewContainer(view)
-
         }
+
 
         calendarView.monthHeaderBinder = object : MonthHeaderFooterBinder<MonthViewContainer> {
             override fun create(view: View) = MonthViewContainer(view)
             override fun bind(container: MonthViewContainer, month: CalendarMonth) {
-                container.textView.text = "${month.yearMonth.month.name} ${month.year}"
+
+                when (month.yearMonth.month.name) {
+                    "JANUARY" -> container.textView.text = "${month.year}년 1월"
+                    "FEBRUARY" -> container.textView.text = "${month.year}년 2월"
+                    "MARCH" -> container.textView.text = "${month.year}년 3월"
+                    "APRIL" -> container.textView.text = "${month.year}년 4월"
+                    "MAY" -> container.textView.text = "${month.year}년 5월"
+                    "JUNE" -> container.textView.text = "${month.year}년 6월"
+                    "JULY" -> container.textView.text = "${month.year}년 7월"
+                    "AUGUST" -> container.textView.text = "${month.year}년 8월"
+                    "SEPTEMBER" -> container.textView.text = "${month.year}년 9월"
+                    "OCTOBER" -> container.textView.text = "${month.year}년 10월"
+                    "NOVEMBER" -> container.textView.text = "${month.year}년 11월"
+                    "DECEMBER" -> container.textView.text = "${month.year}년 12월"
+                }
             }
+
         }
 
         calendarView.setup(firstMonth, lastMonth, daysOfWeek.first())
@@ -89,6 +109,41 @@ class RecordFragment : PetComsBaseFragment<FragmentRecordBinding>(R.layout.fragm
     inner class DayViewContainer(view: View) : ViewContainer(view) {
 
         val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
+        lateinit var day: CalendarDay
+        val thisDayOwner = DayOwner.THIS_MONTH
+        val previousDayOwner = DayOwner.PREVIOUS_MONTH
+        val nextDayOwner = DayOwner.NEXT_MONTH
+
+
+        init {
+            view.setOnClickListener {
+                val currentSelection = selectedDate
+                /** 각 날짜가 클릭되었을 때 실행되는 Click Listener **/
+                if (day.owner == thisDayOwner) {
+                    // Keep a reference to any previous selection
+                    // in case we overwrite it and need to reload it.
+
+                    if (currentSelection == day.date) {
+                        // If the user clicks the same date, clear selection.
+                        selectedDate = null
+                        // Reload this date so the dayBinder is called
+                        // and we can REMOVE the selection background.
+                        calendarView.notifyDateChanged(currentSelection)
+                    } else {
+                        selectedDate = day.date
+                        // Reload the newly selected date so the dayBinder is
+                        // called and we can ADD the selection background.
+                        calendarView.notifyDateChanged(day.date)
+                        if (currentSelection != null) {
+                            // We need to also reload the previously selected
+                            // date so we can REMOVE the selection background.
+                            calendarView.notifyDateChanged(currentSelection)
+                        }
+                    }
+                }
+
+            }
+        }
 
 
     }
@@ -97,6 +152,18 @@ class RecordFragment : PetComsBaseFragment<FragmentRecordBinding>(R.layout.fragm
         val textView = view.findViewById<TextView>(R.id.headerTextView)
     }
 
+    fun daysOfWeekFromLocale(): Array<DayOfWeek> {
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+        val daysOfWeek = DayOfWeek.values()
+        // Order `daysOfWeek` array so that firstDayOfWeek is at index 0.
+        // Only necessary if firstDayOfWeek is not DayOfWeek.MONDAY which has ordinal 0.
+        if (firstDayOfWeek != DayOfWeek.MONDAY) {
+            val rhs = daysOfWeek.sliceArray(firstDayOfWeek.ordinal..daysOfWeek.indices.last)
+            val lhs = daysOfWeek.sliceArray(0 until firstDayOfWeek.ordinal)
+            return rhs + lhs
+        }
+        return daysOfWeek
+    }
 
 
 }
